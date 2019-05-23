924876702a8c8d5a797d129ebc5fffbbd6f3be2f
Dong Zhang <zdparadise@icloud.com>
2019-05-23 14:48:11 +0800

    modify travis conf

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f "https://{TOKEN}@github.com/xiumu2017.github.io.git" master
+git push -f "https://${TOKEN}@github.com/xiumu2017.github.io.git" master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
